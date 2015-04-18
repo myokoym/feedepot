0ec718065436e6be7b6a86e8b9184dd5c03be3d3
Masafumi Yokoyama <yokoyama@clear-code.com>
2015-04-18 22:43:16 +0900

    test: Add helper for to normalize JSON

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,6 +9,16 @@ class ActiveSupport::TestCase
     FactoryGirl.reload
   end
 
+  def normalize(response_body)
+    normalize_id(normalize_time(response_body))
+  end
+
+  def normalize_id(response_body)
+    response_body.gsub(/"id":\d+/).with_index do |_match, i|
+      "\"id\":#{i + 1}"
+    end
+  end
+
   def normalize_time(response_body)
     pattern = /"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:\.\d{3})?(?:Z|[+-]\d{1,2}:\d{2})"/
     response_body.gsub(pattern) do
