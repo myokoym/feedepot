f3f885d130f080c3822c680f5b31adacdc99f4c8
Masafumi Yokoyama <yokoyama@clear-code.com>
2015-04-18 14:49:22 +0900

    Generate Resource model
        % rails g model Resource \
            text:text \
            is_comment:string \
            is_breakpoint:string \
            created:string \
            category:string \
            description:text \
            url:string \
            html_url:string \
            xml_url:string \
            title:string \
            version:string \
            language:string

---
+++ db/migrate/20150418054911_create_resources.rb
@@ -0,0 +1,20 @@
+class CreateResources < ActiveRecord::Migration
+  def change
+    create_table :resources do |t|
+      t.text :text
+      t.string :is_comment
+      t.string :is_breakpoint
+      t.string :created
+      t.string :category
+      t.text :description
+      t.string :url
+      t.string :html_url
+      t.string :xml_url
+      t.string :title
+      t.string :version
+      t.string :language
+
+      t.timestamps null: false
+    end
+  end
+end
---
+++ test/factories/resources.rb
@@ -0,0 +1,17 @@
+FactoryGirl.define do
+  factory :resource do
+    text "MyText"
+is_comment "MyString"
+is_breakpoint "MyString"
+created "MyString"
+category "MyString"
+description "MyText"
+url "MyString"
+html_url "MyString"
+xml_url "MyString"
+title "MyString"
+version "MyString"
+language "MyString"
+  end
+
+end
